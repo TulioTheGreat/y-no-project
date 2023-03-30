8f696bdbc712ae221d15c50875b50c85ab096cb6
Tulio <91757295+TulioTheGreat@users.noreply.github.com>
2023-03-29 22:17:11 -0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,4 @@
+const videosContainer = document.getElementById("videos")
+
+const key = "AIzaSyBCC3plUwv6eIgAs06mPexoSxvblkxoR3Q"
+const yno_ID = "UCTzmq_nB3j3zJdxY_43AWiw";
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors:{
+      'violet': '#6e44ff',
+      'yellow': '#ffd60a',
+      'white': '#f2f2f2',
+      'black': '#101010',
+    },
+    fontFamily: {
+      'sans': ['poppins'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
